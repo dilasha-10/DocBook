383b91d2bed1a3feaa7555c055f89887e85758c1
awwdrsh <aleenaneupane510@gmail.com>
2026-05-18 22:25:05 +0545

    fix: remove .env dependency, hardcode config defaults

--- config/database.php
+++ config/database.php
@@ -1,17 +1,15 @@
 <?php
-$dotenv = parse_ini_file(__DIR__ . '/../.env');
-define('GROQ_API_KEY', $dotenv['GROQ_API_KEY'] ?? '');
+define('GROQ_API_KEY', '');
 
 // Set timezone to Nepal Time
 date_default_timezone_set('Asia/Kathmandu');
 
 function db_connect() {
-    $dotenv = parse_ini_file(__DIR__ . '/../.env');
-    $host    = $dotenv['DB_HOST']   ?? '127.0.0.1';
-    $port    = $dotenv['DB_PORT']   ?? '3306';
-    $dbname  = $dotenv['DB_NAME']   ?? 'docbook';
-    $user    = $dotenv['DB_USER']   ?? '';
-    $pass    = $dotenv['DB_PASS']   ?? '';
+    $host    = '127.0.0.1';
+    $port    = '3306';
+    $dbname  = 'docbook';
+    $user    = 'root';
+    $pass    = '';
     $charset = 'utf8mb4';
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
     try {
--- config/mail.php
+++ config/mail.php
@@ -1,10 +1,8 @@
 <?php
-$dotenv = parse_ini_file(__DIR__ . '/../.env');
-
-define('MAIL_HOST',       $dotenv['MAIL_HOST']       ?? 'smtp.gmail.com');
-define('MAIL_PORT',       (int)($dotenv['MAIL_PORT'] ?? 587));
-define('MAIL_USERNAME',   $dotenv['MAIL_USERNAME']   ?? '');
-define('MAIL_PASSWORD',   $dotenv['MAIL_PASSWORD']   ?? '');
-define('MAIL_FROM_EMAIL', $dotenv['MAIL_FROM_EMAIL'] ?? '');
-define('MAIL_FROM_NAME',  $dotenv['MAIL_FROM_NAME']  ?? 'DocBook');
-define('MAIL_ENCRYPTION', $dotenv['MAIL_ENCRYPTION'] ?? 'tls');
+define('MAIL_HOST',       'smtp.gmail.com');
+define('MAIL_PORT',       587);
+define('MAIL_USERNAME',   '');
+define('MAIL_PASSWORD',   '');
+define('MAIL_FROM_EMAIL', '');
+define('MAIL_FROM_NAME',  'DocBook');
+define('MAIL_ENCRYPTION', 'tls');
