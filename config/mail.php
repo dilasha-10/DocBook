ce944c058a09ca154b7cd878109419dc9b0c90bf
aleenaneupane0131 <aleenaneupane510@gmail.com>
2026-05-19 21:57:47 +0545

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -1,8 +1,10 @@
 <?php
-define('MAIL_HOST',       'smtp.gmail.com');
-define('MAIL_PORT',       587);
-define('MAIL_USERNAME',   '');
-define('MAIL_PASSWORD',   '');
-define('MAIL_FROM_EMAIL', '');
-define('MAIL_FROM_NAME',  'DocBook');
-define('MAIL_ENCRYPTION', 'tls');
+$dotenv = parse_ini_file(__DIR__ . '/../.env');
+
+define('MAIL_HOST',       $dotenv['MAIL_HOST']       ?? 'smtp.gmail.com');
+define('MAIL_PORT',       (int)($dotenv['MAIL_PORT'] ?? 587));
+define('MAIL_USERNAME',   $dotenv['MAIL_USERNAME']   ?? '');
+define('MAIL_PASSWORD',   $dotenv['MAIL_PASSWORD']   ?? '');
+define('MAIL_FROM_EMAIL', $dotenv['MAIL_FROM_EMAIL'] ?? '');
+define('MAIL_FROM_NAME',  $dotenv['MAIL_FROM_NAME']  ?? 'DocBook');
+define('MAIL_ENCRYPTION', $dotenv['MAIL_ENCRYPTION'] ?? 'tls');
